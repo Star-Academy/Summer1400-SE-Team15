089aec8527f9e11bf269ec3ba05d6db3978ed06a
Arian Hadi <arian.hadi78@gmail.com>
2021-07-28 16:59:28 +0430

    phase03 initiated

---
+++ Phase03/src/InputScanner.java
@@ -0,0 +1,26 @@
+import java.util.Set;
+
+public class InputScanner {
+    final private SearchEngine searchEngine;
+    final private java.util.Scanner scanner;
+
+    public InputScanner(SearchEngine searchEngine){
+        scanner = new java.util.Scanner(System.in);
+        this.searchEngine = searchEngine;
+    }
+    public void ScanInput(){
+        while (true){
+            String query = scanner.nextLine();
+            if(query.equals("--exit")) break;
+            Set<String> results = searchEngine.getResult(query);
+            if(results.isEmpty()){
+                System.out.println("No Result");
+                continue;
+            }
+            for (String result : results){
+                System.out.println(result);
+            }
+        }
+        scanner.close();
+    }
+}
