c2b55e743f9a521a4e059217afa7289729d1aae5
Arian Hadi <arian.hadi78@gmail.com>
2021-07-30 12:48:58 +0430

    invertedindextest and searchenginetest created

--- Phase03/src/test/java/SearchEngineTest.java
+++ Phase03/src/test/java/SearchEngineTest.java
@@ -2,23 +2,26 @@ package test.java;
 
 import main.java.FileReader;
 import main.java.InvertedIndex;
+import main.java.SearchEngine;
 import org.junit.Test;
 
-import java.util.HashSet;
+
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
-public class InvertedIndexTest {
+public class SearchEngineTest {
 
     static final String FOLDER_PATH = "EnglishDataTest";
     static final String STOP_WORDS_PATH = "utilities/stopWords.txt";
 
     @Test
-    public void testTokenize(){
+    public void testSearchEngine(){
         FileReader fileReader = new FileReader(FOLDER_PATH,STOP_WORDS_PATH);
         InvertedIndex invertedIndex = new InvertedIndex(fileReader);
-        HashSet<String> result = invertedIndex.getResultListByWord("poet");
-        assertEquals("inverted index is wrong",result.contains("59652"),true);
+        SearchEngine searchEngine = new SearchEngine(invertedIndex);
+        Set<String> results = searchEngine.getResult("poet");
+        assertEquals("search engine is wrong",results.contains("59652"),true);
     }
 
 }
