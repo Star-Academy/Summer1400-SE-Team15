f0647c7b6a2c9517c7a49fadf792bebec47c5a0b
naser farajzade <13781378.nf@gmail.com>
2021-08-01 22:27:23 +0430

    The tests were written using mock

--- Phase03/src/test/java/InvertedIndexTest.java
+++ Phase03/src/test/java/InvertedIndexTest.java
@@ -1,23 +1,57 @@
 package test.java;
 
 import main.java.FileReader;
+import main.java.FileTuple;
+import main.java.IView;
 import main.java.InvertedIndex;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.stubbing.Answer;
 
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class InvertedIndexTest {
 
     static final String FOLDER_PATH = "EnglishDataTest";
     static final String STOP_WORDS_PATH = "utilities/stopWords.txt";
 
+    static final FileReader fileReader = mock(FileReader.class);
+
     @Test
     public void ShouldTokenizeASample(){
-        FileReader fileReader = new FileReader(FOLDER_PATH,STOP_WORDS_PATH);
+        List<FileTuple> filesContent = new ArrayList<>();
+        List<String> stopWords = new ArrayList<>();
+
+        filesContent.add(new FileTuple("57110","I have a 42 yr old male friend"));
+        filesContent.add(new FileTuple("58043",">This wouldn't happen to be the same thing as chiggers, would it>"));
+        filesContent.add(new FileTuple("59652","This is not an unusual practice if the doctor is also member of a nudist colony--Sir, I admit your gen'ral rulThat every poet"));
+
+        stopWords.add("i");
+        stopWords.add("have");
+        stopWords.add("a");
+        stopWords.add("this");
+        stopWords.add("wouldn't");
+        stopWords.add("to");
+        stopWords.add("be");
+        stopWords.add("the");
+        stopWords.add("as");
+        stopWords.add("is");
+        stopWords.add("not");
+        stopWords.add("an");
+        stopWords.add("if");
+        stopWords.add("of");
+
+        when(fileReader.getFilesContents()).thenReturn(filesContent);
+        when(fileReader.getStopWords()).thenReturn(stopWords);
+
         InvertedIndex invertedIndex = new InvertedIndex(fileReader);
         HashSet<String> result = invertedIndex.getResultListByWord("poet");
         assertEquals(true, result.contains("59652"),"inverted index is wrong");
--- Phase03/src/test/java/SearchEngineTest.java
+++ Phase03/src/test/java/SearchEngineTest.java
@@ -10,18 +10,37 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SearchEngineTest {
 
     static final String FOLDER_PATH = "EnglishDataTest";
     static final String STOP_WORDS_PATH = "utilities/stopWords.txt";
 
+    //static final FileReader fileReader = mock(FileReader.class);
+    static final InvertedIndex invertedIndex = mock(InvertedIndex.class);
+
     @Test
     public void ShouldSearchForProperDocs(){
-        FileReader fileReader = new FileReader(FOLDER_PATH,STOP_WORDS_PATH);
-        InvertedIndex invertedIndex = new InvertedIndex(fileReader);
-        SearchEngine searchEngine = new SearchEngine(invertedIndex);
+        HashSet<String> listOfWord_old = new HashSet<>();
+        listOfWord_old.add("57110");
+        HashSet<String> listOfWord_friend = new HashSet<>();
+        listOfWord_friend.add("57110");
+        HashSet<String> listOfWord_problem = new HashSet<>();
+        listOfWord_problem.add("58045");
+        HashSet<String> listOfWord_doctor = new HashSet<>();
+        listOfWord_doctor.add("59652");
+        HashSet<String> listOfWord_remember = new HashSet<>();
+        listOfWord_remember.add("58045");
 
+        when(invertedIndex.getResultListByWord("old")).thenReturn(listOfWord_old);
+        when(invertedIndex.getResultListByWord("friend")).thenReturn(listOfWord_friend);
+        when(invertedIndex.getResultListByWord("problem")).thenReturn(listOfWord_problem);
+        when(invertedIndex.getResultListByWord("doctor")).thenReturn(listOfWord_doctor);
+        when(invertedIndex.getResultListByWord("remember")).thenReturn(listOfWord_remember);
+
+        SearchEngine searchEngine = new SearchEngine(invertedIndex);
 
         Set<String> results = searchEngine.getResult("old friend +problem +doctor -remember");
         assertEquals(2, results.size(),"size of result is not equal with actual size");
