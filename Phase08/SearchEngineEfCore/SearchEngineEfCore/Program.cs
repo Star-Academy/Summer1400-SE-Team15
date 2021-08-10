592dd24b0f7830924215e4019640e6f1b152c5a0
Arian Hadi <arian.hadi78@gmail.com>
2021-08-10 13:15:17 +0430

    Phase08 initialize

---
+++ Phase08/SearchEngineEfCore/SearchEngineEfCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SearchEngineEfCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
